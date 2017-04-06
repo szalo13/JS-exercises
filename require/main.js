ac9e7497235f7f1d98828a7310f8e8a5864bdb74
szalo13 <kamil_sza@yahoo.pl>
2017-04-06 19:02:04 +0200

    Adding solved task number 10

--- require/main.js
+++ require/main.js
@@ -1,2 +1,16 @@
 //bootstrap
 
+require.config({
+  baseUrl: 'require',
+  paths: {
+    jquery: 'libs/jquery',
+    sidebar: 'modules/sidebar',
+    customScrollbar: 'libs/jquery.mCustomScrollbar'
+  }
+});
+
+require([
+  'sidebar'
+], function(){
+  //initializing app
+});
--- require/modules/sidebar.js
+++ require/modules/sidebar.js
@@ -1,1 +1,28 @@
 
+//Pobieram odpowiednie bibioteki
+require([
+  'sidebar',
+  'customScrollbar'
+], function() {
+
+  //Custom Sidebar
+  let sidebar = $('.sidebar');
+  let lists = sidebar.children('div');
+  let height = $(window).height() / lists.length;
+  let listPercentHeight = 100 / lists.length + "%";
+
+  lists.each(function(){
+    let list = $(this);
+
+    list.addClass('list');
+    list.mCustomScrollbar({
+      setHeight: listPercentHeight,
+      axis: 'x',
+      theme: 'dark',
+      advanced: {
+        updateOnContentResize: true,
+        updateOnSelectorChange:true
+      }
+    });
+  });
+});
